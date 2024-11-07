9b4ee3226c71c08e0857a2297cc08737f1fc9764
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-07 18:08:15 -0500

    Change negative index to .at()
    Realized negative indices don't work...I'm used to Python I guess.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
-
 function playGame() {
     let userSelection = +prompt("Shoot! Select your weapon: Rock (1), Paper (2), or Scissors (3)") - 1;
     let cpuSelection = Math.floor(Math.random()*3);
 
-    if (choices[userSelection] == choices[cpuSelection - 1]) {
+    if (choices[userSelection] == choices.at(cpuSelection - 1)) {
         console.log(`You lose! Computer chose ${choices[cpuSelection]}. You chose ${choices[userSelection]}. ${choices[cpuSelection]} beats ${choices[userSelection]}.`);
         playAgain();
-    } else if (choices[cpuSelection] == choices[userSelection - 1]) {
+    } else if (choices[cpuSelection] == choices.at(cpuSelection - 1)) {
         console.log(`You win! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}. ${choices[userSelection]} beats ${choices[cpuSelection]}.`);
         playAgain();
     } else {
-        console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`)
+        console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`);
         playAgain();
     }
 }
@@ -20,7 +19,7 @@ function playGame() {
 function playAgain() {
     text = "Press OK to play again."
     if (confirm(text) == true) {
-        playGame()
+        playGame();
     }
 }
 
