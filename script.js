7e5a39a66407c3f9c38dab9e3751afc4cf97d09b
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-07 15:03:14 -0500

    Create game logic and structure
    I noticed that in the choices array ["Rock", "Paper", "Scissors"],
    choices[2] is the same as choices[-1], so choice[x] always beats
    choice[x-1]. The conditional block uses that logic to determine the
    winner. Feels like the most elegant way to solve it! I thought about
    hardcoding with && statements or using an array of arrays, but the -1
    thing is simpler and more straightforward. Need to fix the prompt to
    handle type errors.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+const choices = ["Rock", "Paper", "Scissors"];
+
+let userSelection = +prompt("Select your weapon: Rock (1), Paper (2), or Scissors (3)") - 1;
+let cpuSelection = Math.floor(Math.random()*3);
+
+if (choices[userSelection] == choices[cpuSelection - 1]) {
+    console.log(`You lose! Computer chose ${choices[cpuSelection]}. You chose ${choices[userSelection]}. ${choices[cpuSelection]} beats ${choices[userSelection]}.`);
+} else if (choices[cpuSelection] == choices[userSelection - 1]) {
+    console.log(`You win! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}. ${choices[userSelection]} beats ${choices[cpuSelection]}.`);
+} else {
+    console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`)
+}
