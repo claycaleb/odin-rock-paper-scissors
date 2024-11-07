5679e1108582f0a4244d6072b9f2d8943d0d4834
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-07 17:13:17 -0500

    Add play again feature

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
-let userSelection = +prompt("Select your weapon: Rock (1), Paper (2), or Scissors (3)") - 1;
-let cpuSelection = Math.floor(Math.random()*3);
 
-if (choices[userSelection] == choices[cpuSelection - 1]) {
-    console.log(`You lose! Computer chose ${choices[cpuSelection]}. You chose ${choices[userSelection]}. ${choices[cpuSelection]} beats ${choices[userSelection]}.`);
-} else if (choices[cpuSelection] == choices[userSelection - 1]) {
-    console.log(`You win! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}. ${choices[userSelection]} beats ${choices[cpuSelection]}.`);
-} else {
-    console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`)
+function playGame() {
+    let userSelection = +prompt("Shoot! Select your weapon: Rock (1), Paper (2), or Scissors (3)") - 1;
+    let cpuSelection = Math.floor(Math.random()*3);
+
+    if (choices[userSelection] == choices[cpuSelection - 1]) {
+        console.log(`You lose! Computer chose ${choices[cpuSelection]}. You chose ${choices[userSelection]}. ${choices[cpuSelection]} beats ${choices[userSelection]}.`);
+        playAgain();
+    } else if (choices[cpuSelection] == choices[userSelection - 1]) {
+        console.log(`You win! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}. ${choices[userSelection]} beats ${choices[cpuSelection]}.`);
+        playAgain();
+    } else {
+        console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`)
+        playAgain();
+    }
 }
+
+function playAgain() {
+    text = "Press OK to play again."
+    if (confirm(text) == true) {
+        playGame()
+    }
+}
+
+playGame();
