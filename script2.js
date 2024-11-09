c3eed2036bd649b2d43d400508fc1e59ee5e30a4
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-09 16:52:28 -0500

    Remove if statement

--- script2.js
+++ script2.js
@@ -1,17 +1,9 @@
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random()*3);
-
     switch (computerChoice) {
         case 0: return "rock";
         case 1: return "paper";
         case 2: return "scissors";
-    }
-    if (computerChoice == 0) {
-        return "rock";
-    } else if (computerChoice == 1) {
-        return "paper";
-    } else if (computerChoice == 2) {
-        return "scissors";
     }
 }
 
