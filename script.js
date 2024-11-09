bd75ad9f61f5e257d46265b1497efe90ed7f02cb
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-09 14:20:25 -0500

    Add round system, invalid input function, win/lose

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
+let humanScore = 0;
 
-function playGame() {
-    let userSelection = +prompt("Shoot! Select your weapon: Rock (1), Paper (2), or Scissors (3)") - 1;
-    let cpuSelection = Math.floor(Math.random()*3);
-
-    if (choices[userSelection] == choices.at(cpuSelection - 1)) {
-        console.log(`You lose! Computer chose ${choices[cpuSelection]}. You chose ${choices[userSelection]}. ${choices[cpuSelection]} beats ${choices[userSelection]}.`);
-        playAgain();
-    } else if (choices[cpuSelection] == choices.at(userSelection - 1)) {
-        console.log(`You win! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}. ${choices[userSelection]} beats ${choices[cpuSelection]}.`);
-        playAgain();
-    } else {
-        console.log(`It's a tie! You chose ${choices[userSelection]}. Computer chose ${choices[cpuSelection]}.`);
+function checkWin() {
+    if (humanScore == 3) {
+        alert("You won!");
         playAgain();
     }
+
+    if (computerScore == 3) {
+        alert("You lost!");
+        playAgain();
+    }
+}
+
+function checkIfInvalidInput(input) {
+    if (input >= 1 && input <= 3) {
+        return input;
+    } else {
+        input = prompt("Invalid input. Try again. Select your weapon: Rock (1), Paper (2), or Scissors (3)");
+        return checkIfInvalidInput(input);
+    }
+}
+
+function playRound() {
+    let input = prompt("Shoot! Select your weapon: Rock (1), Paper (2), or Scissors (3)");
+
+    input = checkIfInvalidInput(input);
+    
+    let humanSelection = +input - 1;
+    let computerSelection = Math.floor(Math.random()*3);
+
+    let humanChoice = choices[humanSelection];
+    let computerChoice = choices[computerSelection];
+
+    if (humanChoice == choices.at(computerSelection - 1)) {
+        computerScore++;
+        alert(`You lose the round! Computer chose ${computerChoice}. You chose ${humanChoice}. ${computerChoice} beats ${humanChoice}.\nYou: ${humanScore} | Computer: ${computerScore}`);
+        checkWin();
+        playRound();
+    } else if (computerChoice == choices.at(humanSelection - 1)) {
+        humanScore++
+        alert(`You win the round! You chose ${humanChoice}. Computer chose ${computerChoice}. ${humanChoice} beats ${computerChoice}.\nYou: ${humanScore} | Computer: ${computerScore}`);
+        checkWin();
+        playRound();
+    } else if (humanSelection == computerSelection) {
+        alert(`It's a tie! You chose ${humanChoice}. Computer chose ${computerChoice}.\nYou: ${humanScore} | Computer: ${computerScore}`);
+        playRound();
+    } 
 }
 
 function playAgain() {
     text = "Press OK to play again."
     if (confirm(text) == true) {
-        playGame();
+        computerScore = 0;
+        humanScore = 0;
+        playRound();
     }
 }
 
-playGame();
+playRound();
