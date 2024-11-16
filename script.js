413850aec175b2f6f19ddb809c8b7a3eb45956f8
Caleb Clay <48763589+claycaleb@users.noreply.github.com>
2024-11-16 16:05:53 -0500

    Add buttons and button selector function

--- script.js
+++ script.js
@@ -7,70 +7,13 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt('Shoot! Type "rock", "paper", or "scissors" and click OK.');
+let humanScore = 0;
+let computerScore = 0;
 
-    try {
-        humanChoice = humanChoice.toLowerCase();
-    } catch (TypeError) {
-        console.log("Invalid input");
-        return getHumanChoice();
+function checkScore() {
+    if (!(humanScore === 5) || !(computerScore === 5)   ) {
+        return;
     }
-
-    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
-        return humanChoice;
-    } else {
-        console.log("Invalid input");
-        return getHumanChoice();
-    }
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-       
-        if ((computerChoice == "rock" && humanChoice == "paper") || 
-        (computerChoice == "paper" && humanChoice == "scissors") || 
-        (computerChoice == "scissors" && humanChoice == "rock")) {
-            humanScore++;
-            console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You win the round!`);
-            console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`)
-            return;
-        } else if ((humanChoice == "rock" && computerChoice == "paper") || 
-        (humanChoice == "paper" && computerChoice == "scissors") || 
-        (humanChoice == "scissors" && computerChoice == "rock")) {
-            computerScore++;
-            console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You lose the round!`);
-            console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`)
-            return;
-        } else if (humanChoice == computerChoice) {
-            console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. It's a tie.`)
-            console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`)
-            return;
-        }
-    }
-
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
 
     if (humanScore > computerScore) {
         console.log("You win the game!");
@@ -78,7 +21,40 @@ function playGame() {
         console.log("You tied!");
     } else {
         console.log("You lose!")
-    }
-}
+    };
+};
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+    
+    if ((computerChoice == "rock" && humanChoice == "paper") || 
+    (computerChoice == "paper" && humanChoice == "scissors") || 
+    (computerChoice == "scissors" && humanChoice == "rock")) {
+        humanScore++;
+        console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You win the round!`);
+        console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`);
+        checkScore();
+        return;
+    } else if ((humanChoice == "rock" && computerChoice == "paper") || 
+    (humanChoice == "paper" && computerChoice == "scissors") || 
+    (humanChoice == "scissors" && computerChoice == "rock")) {
+        computerScore++;
+        console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You lose the round!`);
+        console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`);
+        checkScore();
+        return;
+    } else if (humanChoice == computerChoice) {
+        console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. It's a tie.`)
+        console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`);
+        checkScore();
+        return;
+    }
+};
+
+selectionButtons = document.querySelectorAll("#playerButtons > button");
+
+selectionButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+   });
+});
+
